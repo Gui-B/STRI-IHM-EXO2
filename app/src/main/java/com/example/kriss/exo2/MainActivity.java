596525a16a892dbcf9d62281d87036334f51b73c
guillaumeBoulic <boulic.guillaume@gmail.com>
2016-11-08 11:30:47 +0100

    exo 2 finish ?

--- app/src/main/java/com/example/kriss/exo2/MainActivity.java
+++ app/src/main/java/com/example/kriss/exo2/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.kriss.exo2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    String[] myTextStrings= new String[] {"invoice1", "invoice2", "invoice3"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listview= (ListView) findViewById(R.id.listExo);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myTextStrings);
+        listview.setAdapter(adapter);
     }
+
 }
